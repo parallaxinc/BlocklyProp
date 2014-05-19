5f3b40694cd6d202a3c18e5e7cff87da2560b047
Michel Lampo <michel@creatingfuture.eu>
2014-05-19 22:33:16 +0200

    Prop-c blocks for logic, procedures and variables

--- blockly/generators/propc.js
+++ blockly/generators/propc.js
@@ -63,7 +63,7 @@ Blockly.propc.ORDER_ASSIGNMENT = 15;    // := *= /= ~/= %= += -= <<= >>= &= ^= |
 Blockly.propc.ORDER_NONE = 99;          // (...)
 
 /*
- * Spin Board profiles
+ * propc Board profiles
  *
  */
 var profile = {
@@ -159,8 +159,8 @@ Blockly.propc.init = function() {
         for (var x = 0; x < variables.length; x++) {
             var varName = Blockly.propc.variableDB_.getDistinctName(variables[x],
                     Blockly.Variables.NAME_TYPE);
-            defvars[x] = '  ' + '{{$var_type_' + variables[x].name + '}} ' +
-                    varName + '\n';
+            defvars[x] = '' + '{{$var_type_' + variables[x].name + '}} ' +
+                    varName + ';\n';
         }
         Blockly.propc.definitions_['variables'] = defvars.join('\n');
     }
@@ -172,10 +172,7 @@ Blockly.propc.init = function() {
  * @return {string} Completed code.
  */
 Blockly.propc.finish = function(code) {
-    // Indent every line.
-    code = '  ' + code.replace(/\n/g, '\n  ');
-    code = code.replace(/\n\s+$/, '\n');
-    code = 'int main() {\n' + code + '\n}';
+
 
     // Convert the definitions dictionary into a list.
     var imports = [];
@@ -189,7 +186,7 @@ Blockly.propc.finish = function(code) {
         } else if (def.match(/^PUB/)) {
             methods.push(def);
         } else if (def.match(/^OBJ/)) {
-            objects.push('  ' + def.substring(3));
+            objects.push('' + def.substring(3));
         } else {
             definitions.push(def);
         }
@@ -198,8 +195,8 @@ Blockly.propc.finish = function(code) {
     for (var def in definitions) {
         for (var variable in Blockly.propc.vartype_) {
             if (definitions[def].indexOf("{{$var_type_" + variable + "}}") > -1) {
-                if (Blockly.Spin.vartype_[variable] !== 'LOCAL') {
-                    definitions[def] = definitions[def].replace("{{$var_type_" + variable + "}}", Blockly.Spin.vartype_[variable]);
+                if (Blockly.propc.vartype_[variable] !== 'LOCAL') {
+                    definitions[def] = definitions[def].replace("{{$var_type_" + variable + "}}", Blockly.propc.vartype_[variable]);
                 } else {
                     definitions[def] = definitions[def].replace("{{$var_type_" + variable + "}} " + variable, "");
                 }
@@ -209,20 +206,28 @@ Blockly.propc.finish = function(code) {
     }
 
     for (var stack in Blockly.propc.stacks_) {
-        definitions.push('  ' + Blockly.propc.stacks_[stack]);
+        definitions.push(Blockly.propc.stacks_[stack]);
     }
 
     // Convert the setups dictionary into a list.
     var setups = [];
     for (var name in Blockly.propc.setups_) {
-        setups.push(Blockly.propc.setups_[name]);
+        setups.push('  ' + Blockly.propc.setups_[name]);
     }
-    setups.push('Start');
+//    setups.push('Start');
 
     var OBJ = (objects.length > 0) ? '\n\nOBJ\n' + objects.join('\n') + '\n' : '';
 
-    var allDefs = imports.join('\n') + '\n\n' + definitions.join('\n') + '\n\n'; //int main() {\n  ' + setups.join('\n  ') + '\n\n';
+    var allDefs = imports.join('\n') + '\n\n' + definitions.join('\n') + '\n\n'; //int main() {\n  ' +
+    var varInits = setups.join('\n') + '\n';
     //   var setup = 'CON\n  _clkmode = xtal1 + pll16x\n  _xinfreq = 5_000_000\n\n';
+
+    // Indent every line.
+    code = '  ' + code.replace(/\n/g, '\n  ');
+    code = code.replace(/\n\s+$/, '\n');
+    code = 'int main() {\n' + varInits + code + '\n}';
+
+
     return allDefs.replace(/\n\n+/g, '\n\n').replace(/\n*$/, '\n\n\n') + code + '\n\n' + methods.join('\n');
 };
 
@@ -237,7 +242,7 @@ Blockly.propc.scrubNakedValue = function(line) {
 };
 
 /**
- * Encode a string as a properly escaped Spin string, complete with quotes.
+ * Encode a string as a properly escaped propc string, complete with quotes.
  * @param {string} string Text to encode.
  * @return {string} Prop-c string.
  * @private
@@ -256,7 +261,7 @@ Blockly.propc.quote_ = function(string) {
  * Handles comments for the specified block and any connected value blocks.
  * Calls any statements following this block.
  * @param {!Blockly.Block} block The current block.
- * @param {string} code The Spin code created for this block.
+ * @param {string} code The propc code created for this block.
  * @return {string} Prop-c code with comments and subsequent blocks added.
  * @this {Blockly.CodeGenerator}
  * @private
