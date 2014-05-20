c424037232e3b77305926ae2c24562e9567e45f4
Michel Lampo <michel@creatingfuture.eu>
2014-05-20 20:41:48 +0200

    Prop-C pin read-write and bit math

--- blockly/generators/propc/base.js
+++ blockly/generators/propc/base.js
@@ -43,6 +43,48 @@ Blockly.Language.inout_digital_write = {
     }
 };
 
+Blockly.Language.inout_digital_read = {
+    category: 'In/Out',
+    helpUrl: 'help/block-digitalpin.html#read',
+    init: function() {
+        this.setColour(230);
+        this.appendDummyInput("")
+                .appendTitle("DigitalRead PIN#")
+                .appendTitle(new Blockly.FieldDropdown(profile.default.digital), "PIN");
+        this.setOutput(true, Boolean);
+        this.setTooltip('');
+    }
+};
+
+Blockly.Language.inout_digital_write_pin = {
+    category: 'In/Out',
+    helpUrl: 'help/block-digitalpin.html#write-pin',
+    init: function() {
+        this.setColour(230);
+        this.appendDummyInput("").appendTitle("DigitalWrite PIN#");
+        this.appendValueInput('PIN').setCheck(Number);
+        this.appendDummyInput("").appendTitle("Stat")
+                .appendTitle(new Blockly.FieldDropdown([["HIGH", "1"], ["LOW", "0"]]), "STAT");
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('Write digital value to a specific Port');
+        this.setInputsInline(true);
+    }
+};
+
+Blockly.Language.inout_digital_read_pin = {
+    category: 'In/Out',
+    helpUrl: 'help/block-digitalpin.html#read-pin',
+    init: function() {
+        this.setColour(230);
+        this.appendDummyInput("").appendTitle("DigitalRead PIN#");
+        this.appendValueInput('PIN').setCheck(Number);
+        this.setOutput(true, Boolean);
+        this.setTooltip('');
+        this.setInputsInline(true);
+    }
+};
+
 Blockly.Language.base_delay = {
     category: 'Control',
     helpUrl: 'help/block-delay.html',
@@ -70,6 +112,30 @@ Blockly.propc.inout_digital_write = function() {
     } else {
         return 'low(' + dropdown_pin + ');\n';
     }
+};
+
+Blockly.propc.inout_digital_read = function() {
+    var dropdown_pin = this.getTitleValue('PIN');
+    //  Blockly.Spin.setups_['setup_input_' + dropdown_pin] = 'pinMode(' + dropdown_pin + ', INPUT);';
+    var code = 'input(' + dropdown_pin + ')';
+    return [code, Blockly.propc.ORDER_ATOMIC];
+};
+
+Blockly.propc.inout_digital_write_pin = function() {
+    var dropdown_pin = Blockly.propc.valueToCode(this, 'PIN', Blockly.propc.ORDER_UNARY_PREFIX) || '0';
+    var dropdown_stat = this.getTitleValue('STAT');
+    if (dropdown_stat == 1) {
+        return 'high(' + dropdown_pin + ');\n';
+    } else {
+        return 'low(' + dropdown_pin + ');\n';
+    }
+};
+
+Blockly.propc.inout_digital_read_pin = function() {
+    var dropdown_pin = Blockly.propc.valueToCode(this, 'PIN', Blockly.propc.ORDER_UNARY_PREFIX) || '0';
+    //  Blockly.Spin.setups_['setup_input_' + dropdown_pin] = 'pinMode(' + dropdown_pin + ', INPUT);';
+    var code = 'input(' + dropdown_pin + ')';
+    return [code, Blockly.propc.ORDER_ATOMIC];
 };
 
 Blockly.propc.base_delay = function() {
