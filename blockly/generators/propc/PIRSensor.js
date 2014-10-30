5e03c09fce7b61cca30127322c697ffe2fe9978c
Vale <valetolpegin@gmail.com>
2014-10-30 19:11:05 -0400

    Update PIRSensor.js

--- blockly/generators/propc/PIRSensor.js
+++ blockly/generators/propc/PIRSensor.js
@@ -8,14 +8,20 @@ if ( !Blockly.Language )
 //PIR sensor blocks 
 Blockly.Language.PIR_Sensor = {
   category: 'Sensors',
-  help_url: '',
-  this.appendDummyInput( "" )
+  helpUrl: '',
+  init: function() {
+    this.appendDummyInput( "" )
     .appendTitle( "PIR Sensor" )
     .appendTitle( "Pin" )
     .appendDropdownField( Blockly.DropdownField( profile.default.digital ), "PIN" );
   this.setNextStatement( false, null );
-  this.setPreviousStatement( false, null );
+  this.setPreviousStatement( false, null ); 
+  }
 };
 
 //Get generators
-Blockly.
+Blockly.propc = Blockly.Generators.propc( "" );
+
+Blockly.Language.PIR_Sensor = {
+  return '';
+}
