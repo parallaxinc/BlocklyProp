78361788b29504d1a728f1fedbcab6ad33f13f11
Vale <valetolpegin@gmail.com>
2014-10-31 17:27:31 -0400

    Update PIRSensor.js

--- blockly/generators/propc/PIRSensor.js
+++ blockly/generators/propc/PIRSensor.js
@@ -1,35 +1,3 @@
-/*
-'use strict';
-
-
-if ( !Blockly.Language )
-  Blockly.Language = {};
-
-
-//Joystick block
-Blockly.Language.joystick_input = {
-  category: 'Sensors',
-  help_url: '',
-  init: function() {
-    this.setColour( 210 );
-    this.appendDummyInput( "" )
-      .appendTitle( "Joystick" )
-      .appendTitle( "A/D PIN#" )
-      .appendTitle( new Blockly.FieldDropdown( [["0", "0"], ["1", "1"], ["2", "2"], ["3", "3"]] ), "PIN" );
-    this.setPreviousStatement( true, null );
-    this.setNextStatement( true, null );
-  }
-};
-
-
-//Define generators
-Blockly.propc = Blockly.Generator.get( 'propc' );
-
-Blockly.propc.joystick_input = function() {
-  return '';
-};
-*/
-
 'use strict';
 
 if ( !Blockly.Language )
@@ -54,5 +22,8 @@ Blockly.Language.PIR_Sensor = {
 Blockly.propc = Blockly.Generator.get( 'propc' );
 
 Blockly.propc.PIR_Sensor = function() {
-  return '';
+  var pin = this.getTitleValue( 'PIN' );
+  
+  var code = 'input( ' + pin + ' )';
+  return code;
 };
