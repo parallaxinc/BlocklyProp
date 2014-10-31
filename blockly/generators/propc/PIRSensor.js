f9c10615174999ee258ed643dea5a7866066586a
Vale <valetolpegin@gmail.com>
2014-10-31 16:58:01 -0400

    Update PIRSensor.js

--- blockly/generators/propc/PIRSensor.js
+++ blockly/generators/propc/PIRSensor.js
@@ -1,3 +1,35 @@
+/*
+'use strict';
+
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+
+
+//Joystick block
+Blockly.Language.joystick_input = {
+  category: 'Sensors',
+  help_url: '',
+  init: function() {
+    this.setColour( 210 );
+    this.appendDummyInput( "" )
+      .appendTitle( "Joystick" )
+      .appendTitle( "A/D PIN#" )
+      .appendTitle( new Blockly.FieldDropdown( [["0", "0"], ["1", "1"], ["2", "2"], ["3", "3"]] ), "PIN" );
+    this.setPreviousStatement( true, null );
+    this.setNextStatement( true, null );
+  }
+};
+
+
+//Define generators
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+Blockly.propc.joystick_input = function() {
+  return '';
+};
+*/
+
 'use strict';
 
 if ( !Blockly.Language )
@@ -8,7 +40,7 @@ Blockly.Language.PIR_Sensor = {
   category: 'Sensors',
   helpUrl: '',
   init: function() {
-    this.setColour( 314 );
+    this.setColour( 300 );
     this.appendDummyInput( "" )
       .appendTitle( "PIR Sensor" )
       .appendTitle( "Pin" )
@@ -19,7 +51,7 @@ Blockly.Language.PIR_Sensor = {
 };
 
 //Get generators
-Blockly.propc = Blockly.Generators.get( "propc" );
+Blockly.propc = Blockly.Generator.get( "propc" );
 
 Blockly.propc.PIR_Sensor = function() {
   return '';
