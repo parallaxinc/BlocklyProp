8215b556cef8927979a5380398f84c043ed2ac91
Vale <valetolpegin@gmail.com>
2014-11-04 10:12:02 -0500

    Update cog.js

--- blockly/generators/propc/cog.js
+++ blockly/generators/propc/cog.js
@@ -3,3 +3,19 @@
 This file will contain support for multi cog use in Propeller C
 
 */
+'use strict';
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+  
+  
+Blockly.Language.cog_new = {
+  //Add cog block here
+}
+
+//get generators
+Blockly.propc = Blockly.generators.get( 'propc' );
+
+Blockly.propc.cog_new = function() {
+  //Add cog compilation here
+}
