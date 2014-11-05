048caf7ae1bf89d640134406865b1bf30bfb7814
Vale <valetolpegin@gmail.com>
2014-11-05 14:26:15 -0500

    Update cog.js

--- blockly/generators/propc/cog.js
+++ blockly/generators/propc/cog.js
@@ -1,37 +1,4 @@
 /*
-Blockly.Language.cog_new = {
-    // Repeat forever.
-    category: Blockly.LANG_CATEGORY_COG,
-    helpUrl: 'help/block-cognew.html',
-    init: function() {
-        this.setColour(120);
-        this.appendDummyInput()
-                .appendTitle('Cognew ');
-
-        this.appendDummyInput().appendTitle('Stacksize ').appendTitle(new Blockly.FieldTextInput("48", Blockly.Language.math_number.validator), "STACK_SIZE");
-
-        this.appendStatementInput("METHOD")
-                .appendTitle("Method");
-        this.setPreviousStatement(true);
-        this.setNextStatement(true);
-        this.setInputsInline(true);
-        // this.setTooltip(Blockly.LANG_CONTROLS_REPEAT_TOOLTIP);
-    }
-};
-
-Blockly.Spin.cog_new = function() {
-    var method = Blockly.Spin.statementToCode(this, 'METHOD');
-    method = method.replace("  ", "").replace("\n", "");
-    var stackSize = this.getTitleValue('STACK_SIZE');
-    var stackName = 'Stack' + Blockly.Spin.stacks_.length;
-
-    Blockly.Spin.stacks_.push('long ' + stackName + '[' + stackSize + ']');
-
-    var code = 'cognew(' + method + ', @' + stackName + ')\n';
-    return code;
-};
-*/
-/*
 
 This file contains support for multi cog use in Propeller C
 
@@ -47,7 +14,7 @@ Blockly.Language.cog_new = {
   helpUrl: '',
   init: function() {
     this.setColour( 120 );
-    this.appendDummyInput( "" )
+    this.appendDummyInput()
       .appendTitle( "cognew" );
     this.appendValueInput( "STACK_SIZE", Number )
       .appendTitle( "Stacksize" )
@@ -67,6 +34,6 @@ Blockly.propc.cog_new = function() {
   method = method.replace( "  ", "" ).replace( "\n", "" );
   var stackSize = Blockly.propc.valueToCode( this, 'STACK_SIZE', Blockly.propc.ORDER_ATOMIC ) || '10';
   
-  var code = 'cog_run( &' + method + ', ' + stacksize + ' );';
+  var code = 'cog_run( &' + method + ', ' + stackSize + ' );';
   return code;
 };
