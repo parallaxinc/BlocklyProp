f40b8fddb56e800d472cca7ada00019f9519fac9
Vale <valetolpegin@gmail.com>
2014-11-05 16:26:34 -0500

    Update cog.js

--- blockly/generators/propc/cog.js
+++ blockly/generators/propc/cog.js
@@ -21,6 +21,7 @@ Blockly.Language.cog_new = {
       .setCheck( Number );
     this.appendStatementInput( "METHOD" )
       .appendTitle( "Method" );
+    this.setInputsInline( true );
     this.setPreviousStatement( true, null );
     this.setNextStatement( true, null );
   }
@@ -34,9 +35,9 @@ Blockly.propc.cog_new = function() {
   method = method.replace( "  ", "" ).replace( "\n", "" ).replace( "()", "" ).replace( ";", "" );
   var stackSize = Blockly.propc.valueToCode( this, 'STACK_SIZE', Blockly.propc.ORDER_ATOMIC ) || '10';
   
-  //var stackName = 'Stack' + Blockly.propc.stacks_.length;
-  //Blockly.propc.stacks_.push('long ' + stackName + '[' + stackSize + ']');
+  var stackName = 'Stack' + Blockly.propc.stacks_.length;
+  Blockly.propc.stacks_.push('long ' + stackName + '[' + stackSize + '];');
   
-  var code = 'cog_run( &' + method + ', ' + stackSize + ' );';
+  var code = 'cog_run( &' + method + ', ' + stackName + ' );';
   return code;
 };
