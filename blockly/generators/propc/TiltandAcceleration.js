b17fb73037ef4140787e2120a75ea265b19c0c0a
Vale <valetolpegin@gmail.com>
2014-11-06 13:28:15 -0500

    Create TiltandAcceleration.js

---
+++ blockly/generators/propc/TiltandAcceleration.js
@@ -0,0 +1,14 @@
+/*
+
+This file contains support for the Tilt and Acceleration sensors
+
+Author: valetolpegin@gmail.com
+*/
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+  
+//Using the MX2125 Acceleration sensor
+Blockly.Language.MX2125_acceleration = {
+  //Add acceleration sensor block code here
+}
