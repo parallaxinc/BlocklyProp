f64f5269b47f2eb59c8368e407bd4b3e68bd219b
Vale <valetolpegin@gmail.com>
2014-11-08 11:20:44 -0500

    Added v1.0 code

--- blockly/generators/propc/TiltandAcceleration.js
+++ blockly/generators/propc/TiltandAcceleration.js
@@ -9,6 +9,50 @@ if ( !Blockly.Language )
   Blockly.Language = {};
   
 //Using the MX2125 Acceleration sensor
-Blockly.Language.MX2125_acceleration = {
-  //Add acceleration sensor block code here
-}
+Blockly.Language.MX2125_acceleration_xaxis = {
+  category: 'Sensors',
+  helpUrl: '',
+  init: function() {
+    this.appendDummyInput( "" )
+      .appendTitle( "X-axis PIN#" )
+      .appendTitle( new Blockly.FieldDropdown( profile.default.Digital ), "PINX" );
+    this.setOutput( true, Number );
+    this.setInputsInline( true );
+  }
+};
+
+Blockly.Language.MX2125_acceleration_yaxis = {
+  category: 'Sensors',
+  helpUrl: '',
+  init: function() {
+    this.appendDummyInput( "" )
+      .appendTitle( "Y-axis PIN#" )
+      .appendTitle( new Blockly.FieldDropdown( profile.default.Digital ), "PINY" );
+    this.setOutput( true, Number );
+    this.setInputsInline( true );
+  }
+};
+
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+Blockly.propc.MX2125_acceleration_xaxis = function() {
+  var pin = this.getTitleValue( 'PINX' );
+  
+  if ( Blockly.propc.definitions_[ "include mx2125" ] === undefined )
+  {
+    Blockly.propc.definitions_[ "include mx2125" ] = '#include "mx2125.h";';
+  }
+  
+  return 'mx_tilt( ' + pin + ' );\n';
+};
+
+Blockly.propc.MX2125_acceleration_xaxis = function() {
+  var pin = this.getTitleValue( 'PINY' );
+  
+  if ( Blockly.propc.definitions_[ "include mx2125" ] === undefined )
+  {
+    Blockly.propc.definitions_[ "include mx2125" ] = '#include "mx2125.h";';
+  }
+  
+  return 'mx_tilt( ' + pin + ' );\n';
+};
