c6fd07c773ad32d08c069cb53ee54088ef1ff9ed
Vale <valetolpegin@gmail.com>
2014-11-09 13:09:24 -0500

    Edited

--- blockly/generators/propc/TiltandAcceleration.js
+++ blockly/generators/propc/TiltandAcceleration.js
@@ -15,8 +15,9 @@ Blockly.Language.MX2125_acceleration_xaxis = {
   category: 'Sensors',
   helpUrl: '',
   init: function() {
+    this.setColour( 210 );
     this.appendDummyInput( "" )
-      .appendTitle( "X-axis PIN#" )
+      .appendTitle( "MX2125 X-axis PIN#" )
       .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PINX" );
     this.setOutput( true, Number );
   }
@@ -26,8 +27,9 @@ Blockly.Language.MX2125_acceleration_yaxis = {
   category: 'Sensors',
   helpUrl: '',
   init: function() {
+    this.setColour( 210 );
     this.appendDummyInput( "" )
-      .appendTitle( "Y-axis PIN#" )
+      .appendTitle( "MX2125 Y-axis PIN#" )
       .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PINY" );
     this.setOutput( true, Number );
   }
@@ -40,23 +42,23 @@ Blockly.propc = Blockly.Generator.get( 'propc' );
 Blockly.propc.MX2125_acceleration_xaxis = function() {
   var pin = this.getTitleValue( 'PINX' );
   
-  if ( Blockly.propc.definitions_[ "include mx2125" ] === undefined )
+  if ( Blockly.propc.definitions_[ "include_mx2125" ] === undefined )
   {
-    Blockly.propc.definitions_[ "include mx2125" ] = '#include "mx2125.h";';
+    Blockly.propc.definitions_[ "include_mx2125" ] = '#include "mx2125.h";';
   }
   
-  var code = 'mx_tilt( ' + pin + ' );\n';
+  var code = 'mx_tilt( ' + pin + ' )';
   return [ code, Blockly.ORDER_ATOMIC ];
 };
 
 Blockly.propc.MX2125_acceleration_xaxis = function() {
   var pin = this.getTitleValue( 'PINY' );
   
-  if ( Blockly.propc.definitions_[ "include mx2125" ] === undefined )
+  if ( Blockly.propc.definitions_[ "include_mx2125" ] === undefined )
   {
-    Blockly.propc.definitions_[ "include mx2125" ] = '#include "mx2125.h";';
+    Blockly.propc.definitions_[ "include_mx2125" ] = '#include "mx2125.h";';
   }
   
-  var code = 'mx_tilt( ' + pin + ' );\n';
+  var code = 'mx_tilt( ' + pin + ' )';
   return [ code, Blockly.ORDER_ATOMIC ];
 };
