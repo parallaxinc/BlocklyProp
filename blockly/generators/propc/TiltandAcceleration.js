708adcf9c7ab0ddf445d134cdbd1be7389ffde91
Vale <valetolpegin@gmail.com>
2014-11-10 07:17:04 -0500

    Added "backup" code
    Added PIRSensor code to act as a base. I am going to switch current code into PIRSensor code block and see if that works.

--- blockly/generators/propc/TiltandAcceleration.js
+++ blockly/generators/propc/TiltandAcceleration.js
@@ -4,6 +4,40 @@ This file contains support for the Tilt and Acceleration sensors
 
 Author: valetolpegin@gmail.com
 */
+
+/*
+'use strict';
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+ 
+//PIR sensor blocks 
+Blockly.Language.PIR_Sensor = {
+  category: 'Sensors',
+  helpUrl: '',
+  init: function() {
+    this.setColour( 300 );
+    this.appendDummyInput( "" )
+      .appendTitle( "PIR Sensor" )
+      .appendTitle( "Pin" )
+      .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PIN" );
+    this.setNextStatement( false, null );
+    this.setPreviousStatement( false, null ); 
+    this.setOutput( true, Number );
+  }
+};
+
+//Get generators
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+Blockly.propc.PIR_Sensor = function() {
+  var pin = this.getTitleValue( 'PIN' );
+  
+  var code = 'input( ' + pin + ' )';
+  return [ code, Blockly.propc.ORDER_ATOMIC ];
+};
+*/
+
 'use strict';
 
 if ( !Blockly.Language )
