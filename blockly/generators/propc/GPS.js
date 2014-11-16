74438d5acf2da0b108ead853c4ec5e00103bc85f
Vale <valetolpegin@gmail.com>
2014-11-15 21:07:10 -0500

    Added PAM-7Q module

--- blockly/generators/propc/GPS.js
+++ blockly/generators/propc/GPS.js
@@ -4,3 +4,20 @@
   Author: Vale Tolpegin ( valetolpegin@gmail.com )
   
 */
+
+'use strict';
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+
+//Create GUI blocks for PAM-7Q GPS module
+Blockly.Language.PAM_7Q_Init = {
+  //Add code here for initialization of the PAM_7Q GPS module
+};
+
+//Create code for blocks
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+Blockly.propc.PAM_7Q_Init = function() {
+  //Add code here for initialization of the PAM_7Q GPS module
+};
