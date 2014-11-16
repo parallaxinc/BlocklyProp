5b308fdc49a88887f6d8ca5578e3982cb09969b2
Vale <valetolpegin@gmail.com>
2014-11-16 09:07:25 -0500

    Added etape_rc_time input block

--- blockly/generators/propc/pressure.js
+++ blockly/generators/propc/pressure.js
@@ -10,7 +10,20 @@ if ( !Blockly.Language )
 
 //Creating GUI blocks for eTape liquid sensor
 Blockly.Language.etape_rc_time = {
-  //Add eTape sensor rc time input block
+  category: 'Sensors',
+  helpUrl: '',
+  init: function() {
+    this.setColour( 300 );
+    this.appendDummyInput( "" )
+      .appendTitle( "Pin" )
+      .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PIN" );
+    this.appendDummyInput( "" )
+      .appendTitle( "Put input value in" )
+      .appendTitle( new Blockly.FieldVariable( Blockly.LANG_VARIABLES_GET_ITEM ), 'VAR' );
+    this.setNextStatement( true, null );
+    this.setPreviousStatement( true, null );
+    this.setInputsInline( true );
+  }
 };
 
 //Get generator
@@ -18,5 +31,9 @@ Blockly.propc = Blockly.Generator.get( 'propc' );
 
 //Create code for blocks
 Blockly.propc.etape_rc_time = function() {
-  //Add code for etape rc time input block
+  var pin = this.getTitleValue( 'PIN' );
+  var inputStorage = Blockly.propc.variableDB_.getName( this.getTitleValue( 'VAR' ), Blockly.Variables.NAME_TYPE );
+  
+  var code = 'high( ' + pin + ' );\npause( 1 );\n' + inputStorage + ' = ' + 'rc_time( ' + pin + ', 1 );';
+  return [ code, Blockly.ORDER_ATOMIC ];
 };
