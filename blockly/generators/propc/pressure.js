0743ce518332e257144b999d3324709179798e1c
Vale <valetolpegin@gmail.com>
2014-11-16 08:45:39 -0500

    Added function blocks

--- blockly/generators/propc/pressure.js
+++ blockly/generators/propc/pressure.js
@@ -3,3 +3,20 @@
   
   Author: Vale Tolpegin ( valetolpegin@gmail.com )
 */
+'use strict';
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+
+//Creating GUI blocks for eTape liquid sensor
+Blockly.Language.etape_rc_time = {
+  //Add eTape sensor rc time input block
+};
+
+//Get generator
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+//Create code for blocks
+Blockly.propc.etape_rc_time = function() {
+  //Add code for etape rc time input block
+};
