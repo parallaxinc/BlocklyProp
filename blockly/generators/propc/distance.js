0b2869c7c95f462ce9147b9b5c29504941df5b92
Vale <valetolpegin@gmail.com>
2014-11-15 21:23:52 -0500

    Continued to work on SF02 Laser Rangefinder

--- blockly/generators/propc/distance.js
+++ blockly/generators/propc/distance.js
@@ -3,3 +3,35 @@
   
   Author: Vale Tolpegin ( valetolpegin@gmail.com )
 */
+'use strict';
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+
+//Create GUI blocks for SF02 Laser Rangefinder
+Blockly.Language.SF02_Laser_Rangefinder = {
+  category: 'Sensors',
+  helpUrl: '',
+  init: function() {
+    this.setColour( 300 );
+    this.appendDummyInput( "" )
+      .appendTitle( "Pin" )
+      .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PIN" );
+    this.setNextStatement( false, null );
+    this.setPreviousStatement( false, null );
+    this.setOutput( true );
+  }
+};
+
+//Get generator
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+//Create code for blocks
+Blockly.propc.SF02_Laser_Rangefinder = function() {
+  var pin = this.getTitleValue( 'PIN' );
+  
+  Blockly.propc.definitions_[ "include abvolt" ] = '#include "abvolts.h"';
+  Blockly.propc.setups_['setup_abvolt'] = 'ad_init(21, 20, 19, 18);';
+  
+  var code = '';
+};
