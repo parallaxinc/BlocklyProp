b5716b1a2840bbe63efa160e98095fcacadb5715
Vale <valetolpegin@gmail.com>
2014-11-20 14:27:33 -0500

    Added base code

--- blockly/generators/spin/PIRSensor.js
+++ blockly/generators/spin/PIRSensor.js
@@ -3,3 +3,33 @@
   
   Author: Vale Tolpegin ( valetolpegin@gmail.com )
 */
+'use strict';
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+ 
+//PIR sensor blocks 
+Blockly.Language.PIR_Sensor = {
+  category: 'Sensors',
+  helpUrl: '',
+  init: function() {
+    this.setColour( 300 );
+    this.appendDummyInput( "" )
+      .appendTitle( "PIR Sensor" )
+      .appendTitle( "Pin" )
+      .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PIN" );
+    this.setNextStatement( false, null );
+    this.setPreviousStatement( false, null ); 
+    this.setOutput( true, Number );
+  }
+};
+
+//Get generators
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+Blockly.propc.PIR_Sensor = function() {
+  var pin = this.getTitleValue( 'PIN' );
+  
+  var code = 'input( ' + pin + ' )';
+  return [ code, Blockly.propc.ORDER_ATOMIC ];
+};
