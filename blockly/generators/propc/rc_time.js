212c416c4df4868b5dd647c1dea02b464c5675af
Vale <valetolpegin@gmail.com>
2014-12-05 08:00:09 -0500

    Added base code

--- blockly/generators/propc/rc_time.js
+++ blockly/generators/propc/rc_time.js
@@ -4,4 +4,52 @@
   
   Author: Vale Tolpegin ( valetolpegin@gmail.com )
   
+ *Copyright 2014 Vale Tolpegin.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ 
 */
+'use strict';
+
+if ( !Blockly.Language )
+  Blockly.Language = {};
+
+//Creating GUI blocks for RC charge and discharge
+Blockly.Language.rc_charge = {
+  category: 'Sensors', //SWITCH TO A BETTER FITTING CATEGORY?
+  helpUrl: '',
+  init: function() {
+    this.setColour( 300 );
+    this.appendDummyInput( "" )
+      .appendTitle( "rc charge" )
+      .appendTitle( "Pin" )
+      .appendTitle( new Blockly.FieldDropdown( profile.default.digital ), "PIN" );
+    this.appendValueInput( "", "TIME" )
+      .appendTitle( "Time" );
+    this.setNextStatement( false, null );
+    this.setPreviousStatement( false, null );
+    this.setOutput( true, Number );
+  }
+};
+
+//Get generator
+Blockly.propc = Blockly.Generator.get( 'propc' );
+
+//Create code for blocks
+Blockly.propc.rc_charge = function() {
+  var pin = this.getTitleValue( 'PIN' );
+  
+  var code = 'rc_time( ' + pin + ', 1 );\n';
+  return [ code, Blockly.propc.ORDER_ATOMIC ];
+};
