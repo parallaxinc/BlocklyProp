ed939b8e7c4572d8a1e2c7272216e7ad80121726
michel-cf <michel@creatingfuture.eu>
2015-05-21 22:26:59 +0200

    Reorganize and configuration setup

--- src/main/java/eu/creatingfuture/propeller/blocklyprop/config/ServletConfig.java
+++ src/main/java/eu/creatingfuture/propeller/blocklyprop/config/ServletConfig.java
@@ -10,6 +10,11 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.google.inject.servlet.ServletModule;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.DefaultConfigurationBuilder;
 
 /**
  *
@@ -17,17 +22,34 @@ import com.google.inject.servlet.ServletModule;
  */
 public class ServletConfig extends GuiceServletContextListener {
     
+    private Configuration configuration;
+    
 
     @Override
     protected Injector getInjector() {
+        readConfiguration();
+        
         return Guice.createInjector(new ServletModule(){
 
             @Override
             protected void configureServlets() {
-                install(new PersistenceModule());
+                bind(Configuration.class).toInstance(configuration);
+             //   install(new PersistenceModule());
             }
             
         });
+    }
+    
+    private void readConfiguration() {
+        try {
+            DefaultConfigurationBuilder configurationBuilder = new DefaultConfigurationBuilder(getClass().getResource("/config.xml"));
+            configuration = configurationBuilder.getConfiguration();
+            System.out.println("configuration: " + configuration.getString("context"));
+        } catch (ConfigurationException ce) {
+     //       ce.printStackTrace();
+        } catch (Throwable t) {
+     //       t.printStackTrace();
+        }
     }
     
 }
