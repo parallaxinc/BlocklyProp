96f843aa9c9067087b16ae7c5aeac839a3b3dca0
michel-cf <michel@creatingfuture.eu>
2016-07-09 21:32:47 +0200

    Initial project sharing setup: db + form, no saving

---
+++ src/main/java/com/parallax/server/blocklyprop/db/dao/ProjectSharingDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.parallax.server.blocklyprop.db.dao;
+
+import com.parallax.server.blocklyprop.db.generated.tables.records.ProjectSharingRecord;
+
+/**
+ *
+ * @author Michel
+ */
+public interface ProjectSharingDao {
+
+    ProjectSharingRecord getProject(Long idProject, String accessKey);
+
+}
