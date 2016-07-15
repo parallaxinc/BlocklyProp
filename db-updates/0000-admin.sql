18babdeba94ce56f3ce3f015763680c6051f66d6
michel-cf <michel@creatingfuture.eu>
2016-07-15 20:02:13 +0200

    Reorganize db scripts

---
+++ db-updates/0000-admin.sql
@@ -0,0 +1,5 @@
+CREATE TABLE if not exists `admin` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `script` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8
