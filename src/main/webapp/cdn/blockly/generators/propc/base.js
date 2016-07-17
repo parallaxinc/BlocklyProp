7e98eab36d5ada22b0f2ce472e09fe89dad77c84
Vale <valetolpegin@gmail.com>
2016-07-17 18:03:25 -0400

    Fix #396

--- src/main/webapp/cdn/blockly/generators/propc/base.js
+++ src/main/webapp/cdn/blockly/generators/propc/base.js
@@ -188,5 +188,5 @@ Blockly.Blocks.comment = {
 Blockly.propc.comment = function() {
     var text = this.getFieldValue("COMMENT_TEXT");
 
-    return '// ' + text;
+    return '// ' + text + '\n';
 };
