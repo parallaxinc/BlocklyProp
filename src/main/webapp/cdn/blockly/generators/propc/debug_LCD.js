a2f44b9651298859e98be5f3866aaab8ba1236ff
Matthew Matz <mmatz@parallax.com>
2016-09-02 12:20:52 -0700

    Updates to serial LCD blocks

--- src/main/webapp/cdn/blockly/generators/propc/debug_LCD.js
+++ src/main/webapp/cdn/blockly/generators/propc/debug_LCD.js
@@ -1,3 +1,5 @@
+/* global colorPalette, profile */
+
 /**
  * Visual Blocks Language
  *
@@ -43,25 +45,29 @@ Blockly.Blocks.debug_lcd_init = {
     }
 };
 
-//Blockly.Blocks.debug_lcd_clear = {
-//    init: function () {
-//        this.setColour(colorPalette.getColor('protocols'));
-//        this.appendDummyInput()
-//                .appendField("LCD clear");
-//
-//        this.setPreviousStatement(true, null);
-//        this.setNextStatement(true, null);
-//    }
-//};
+Blockly.Blocks.debug_lcd_music_note = {
+    init: function() {
+        this.setColour(colorPalette.getColor('protocols'));
+        this.appendDummyInput()
+            .appendField("LCD play note")
+            .appendField(new Blockly.FieldDropdown([["C", "223"], ["C#", "224"], ["D", "225"], ["D#", "226"], ["E", "227"], ["F", "228"], ["F#", "229"], ["G", "230"], ["G#", "231"], ["A", "220"], ["A#", "221"], ["B", "222"], ["no note (rest)", "232"]]), "NOTE")
+            .appendField("octave")
+            .appendField(new Blockly.FieldDropdown([["3rd", "215"], ["4th", "216"], ["5th", "217"], ["6th", "218"], ["7th", "219"]]), "OCTAVE")
+            .appendField("length")
+            .appendField(new Blockly.FieldDropdown([["whole (2 s)", "214"], ["half (1 s)", "213"], ["quarter (500 ms)", "212"], ["eigth (250 ms)", "211"], ["sixteenth (125 ms)", "210"], ["thirty-second (63 ms)", "209"], ["sixty-fourth (31 ms)", "208"]]), "LENGTH");
+
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+    }
+};
 
 Blockly.Blocks.debug_lcd_print = {
-    init: function () {
+     init: function () {
         this.setColour(colorPalette.getColor('protocols'));
-        this.appendDummyInput("")
-                .appendField("LCD print")
-                .appendField(quotes.newQuote_(true))
-                .appendField(new Blockly.FieldTextInput(''), 'TEXT')
-                .appendField(quotes.newQuote_(false));
+        this.appendValueInput('MESSAGE')
+            .setCheck('String')
+                .appendField("LCD print text ");
+        this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
     }
@@ -70,12 +76,16 @@ Blockly.Blocks.debug_lcd_print = {
 Blockly.Blocks.debug_lcd_number = {
     init: function () {
         this.setColour(colorPalette.getColor('protocols'));
+        this.appendValueInput('VALUE')
+            .appendField("LCD print number");
         this.appendDummyInput()
-                .appendField("LCD print");
-        this.appendValueInput('NUMBER', Number)
-                .appendField("number")
-                .setCheck('Number');
-
+            .appendField("as")
+            .appendField(new Blockly.FieldDropdown([
+                ['Decimal','DEC'],
+                ['Hexadecimal','HEX'],
+                ['Binary', 'BIN']
+            ]), "FORMAT");
+        this.setInputsInline(true);
         this.setPreviousStatement(true, null);
         this.setNextStatement(true, null);
     }
@@ -120,16 +130,41 @@ Blockly.propc.debug_lcd_init = function () {
 //    return 'writeChar(debug_lcd, 12);\npause(5);\n';
 //};
 
-Blockly.propc.debug_lcd_print = function () {
-    var text = this.getFieldValue('TEXT');
+Blockly.propc.debug_lcd_music_note = function () {
+  var dropdown_note = this.getFieldValue('NOTE');
+  var dropdown_octave = this.getFieldValue('OCTAVE');
+  var dropdown_length = this.getFieldValue('LENGTH');
 
-    return 'dprint(debug_lcd, "' + text + '");\n';
+  var code  = 'writeChar(debug_lcd, ' + dropdown_octave + ');\n';
+      code += 'writeChar(debug_lcd, ' + dropdown_length + ');\n';
+      code += 'writeChar(debug_lcd, ' + dropdown_note + ');\n';
+      
+  return code;
+};
+
+Blockly.propc.debug_lcd_print = function () {
+    var msg = Blockly.propc.valueToCode(this, 'MESSAGE', Blockly.propc.ORDER_NONE);
+    var code = 'dprint(debug_lcd, ' + msg + ');';
+
+    return code;
 };
 
 Blockly.propc.debug_lcd_number = function () {
-    var number = Blockly.propc.valueToCode(this, 'NUMBER', Blockly.propc.ORDER_UNARY_PREFIX) || '0';
+    var value = Blockly.propc.valueToCode(this, 'VALUE', Blockly.propc.ORDER_ATOMIC);
+    var format = this.getFieldValue('FORMAT');
+    Blockly.propc.serial_terminal_ = true;
 
-    return 'dprint(debug_lcd, "' + number + '");\n';
+    var code = 'dprint(debug_lcd, ';
+    if (format === 'BIN') {
+        code += '"%b"';
+    }else if (format === 'HEX') {
+        code += '"%x"';                
+    }else {
+        code += '"%d"';
+    } 
+    
+    code += ', ' + value + ');';
+    return code;
 };
 
 Blockly.propc.debug_lcd_action = function () {
