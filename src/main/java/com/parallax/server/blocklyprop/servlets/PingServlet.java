db3d3aebc65786e08e5ce5b4069931b88336d7bb
Matthew Matz <mmatz@parallax.com>
2017-01-04 14:29:45 -0800

    Add proper HTML to output
    Prevents error from being thrown

--- src/main/java/com/parallax/server/blocklyprop/servlets/PingServlet.java
+++ src/main/java/com/parallax/server/blocklyprop/servlets/PingServlet.java
@@ -24,7 +24,7 @@ public class PingServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("pong");
+        resp.getWriter().write("<html><body>pong</body></html>");
     }
 
 }
