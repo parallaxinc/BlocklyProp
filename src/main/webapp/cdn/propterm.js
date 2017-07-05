c81b622527a061bc5a94e2d47f469f9e2032ddb7
Matthew Matz <mmatz@parallax.com>
2017-07-05 11:56:55 -0700

    Revert terminal back to string-based buffers for now

--- src/main/webapp/cdn/propterm.js
+++ src/main/webapp/cdn/propterm.js
@@ -19,7 +19,7 @@ term_width = Math.floor((parseInt(termPxWide.replace('px', '')) - 25) / term_wid
 var ascii2unicode = ['\u00C7', '\u00FC', '\u00E9', '\u00E2', '\u00E4', '\u00E0', '\u00E5', '\u00E7', '\u00EA', '\u00EB', '\u00E8', '\u00EF', '\u00EE', '\u00EC', '\u00C4', '\u00C5', '\u00C9', '\u00E6', '\u00C6', '\u00F4', '\u00F6', '\u00F2', '\u00FB', '\u00F9', '\u00FF', '\u00D6', '\u00DC', '\u00A2', '\u00A3', '\u00A5', '\u20A7', '\u0192', '\u00E1', '\u00ED', '\u00F3', '\u00FA', '\u00F1', '\u00D1', '\u00AA', '\u00BA', '\u00BF', '\u2310', '\u00AC', '\u00BD', '\u00BC', '\u00A1', '\u00AB', '\u00BB', '\u2591', '\u2592', '\u2593', '\u2502', '\u2524', '\u2561', '\u2562', '\u2556', '\u2555', '\u2563', '\u2551', '\u2557', '\u255D', '\u255C', '\u255B', '\u2510', '\u2514', '\u2534', '\u252C', '\u251C', '\u2500', '\u253C', '\u255E', '\u255F', '\u255A', '\u2554', '\u2569', '\u2566', '\u2560', '\u2550', '\u256C', '\u2567', '\u2568', '\u2564', '\u2565', '\u2559', '\u2558', '\u2552', '\u2553', '\u256B', '\u256A', '\u2518', '\u250C', '\u2588', '\u2584', '\u258C', '\u2590', '\u2580', '\u03B1', '\u00DF', '\u0393', '\u03C0', '\u03A3', '\u03C3', '\u00B5', '\u03C4', '\u03A6', '\u0398', '\u03A9', '\u03B4', '\u221E', '\u03C6', '\u03B5', '\u2229', '\u2261', '\u00B1', '\u2265', '\u2264', '\u2320', '\u2321', '\u00F7', '\u2248', '\u00B0', '\u2219', '\u00B7', '\u221A', '\u207F', '\u00B2', '\u25A0', '\u00A0'];
 
 var echo_trap = '';
-var terminal_buffer = [];
+var terminal_buffer = '';
 
 $(document).ready(function () {
 
@@ -110,7 +110,7 @@ function displayInTerm(str) {
      }
      */
     var termStatus = terminal_buffer.length;
-    terminal_buffer = terminal_buffer.concat(str.split(''));
+    terminal_buffer += str;
     if (termStatus === 0) {
         sendBufferToTerm();
     }
@@ -121,9 +121,9 @@ function sendBufferToTerm() {
         //var o = echo_trap;
         //echo_trap = echo_trap.replace(terminal_buffer[0], '');
         //if (echo_trap.length >= o.length) {
-            updateTermBox(terminal_buffer[0].charCodeAt(0));
+            updateTermBox(terminal_buffer.charCodeAt(0));
         //}
-        terminal_buffer = terminal_buffer.shift();
+        terminal_buffer = terminal_buffer.substr(1);
     }
 }
 
@@ -131,7 +131,7 @@ function updateTermBox(c) {
     cursorGotoY = cursorY;
     if (termSetCursor !== 4)
         cursorGotoX = cursorX;
-    //c = parseInt(c);
+    c = parseInt(c);
     switch (termSetCursor) {
         case 3:
             cursorGotoX = parseInt(c);
@@ -173,7 +173,7 @@ function updateTermBox(c) {
                 case 9:
                     var l = 5 - (cursorX) % 5;
                     for (k = 0; k < l; k++) {
-                        textContainer[cursorY].push(' ');
+                        textContainer[cursorY] += ' ';
                         changeCursor(1, 0);
                     }
                     break;
@@ -232,7 +232,7 @@ function updateTermBox(c) {
                         var the_line = textContainer[cursorY] || '';
                         textContainer[cursorY] = the_line.substr(0, cursorX) + char + the_line.substr(cursorX + 1);
                     } else {
-                        textContainer[cursorY].push(char);
+                        textContainer[cursorY] += char;
                     }
                     changeCursor(1, 0);
                     break;
@@ -302,7 +302,7 @@ function setCursor(cx, cy) {
         }
     }
     while (!textContainer[cy][cx]) {
-        textContainer[cy].push(' ');
+        textContainer[cy] += ' ';
     }
     cursorX = cx;
     cursorY = cy;
