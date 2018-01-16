9cb8e0b347badabd48006874e1e0f1aacd558280
Matthew Matz <mmatz@parallax.com>
2018-01-16 10:18:42 -0800

    Fix graph play/pause/clear behavior

--- src/main/webapp/cdn/blocklyc.js
+++ src/main/webapp/cdn/blocklyc.js
@@ -736,11 +736,19 @@ var graphStartStop = function(action) {
         clearInterval(graph_interval_id);
         graph_interval_id = null;
     } 
-    if (action === 'stop' || action === 'clear') {
-        graph_reset();        
+    if (action === 'stop') {
         graph_paused = false;
+        graph_reset();        
+        graph_play('play');        
+    }
+    if (action === 'clear') {
+        graph_reset();                
     }
     if (action === 'play') {
+        console.log(graph_data.series[0].length);
+        if (graph_data.series[0].length === 0) {
+            graph_reset();
+        }
         graph_paused = false;
         graph_start_playing = true;
     } 
@@ -750,7 +758,6 @@ var graphStartStop = function(action) {
         graph_timestamp_start = 0;
         graph_time_multiplier = 0;
         graph_timestamp_restart = graph_temp_data.slice(-1)[0][0];
-        console.log(graph_timestamp_restart);
     }
 };
 
@@ -943,15 +950,17 @@ function graph_reset() {
     graph_data_ready = false;
 }
 
-function graph_play() {
+function graph_play(setTo) {
     if(document.getElementById('btn-graph-play')) {
         var play_state = document.getElementById('btn-graph-play').innerHTML;
-        if (play_state.indexOf('pause') > -1 || play_state.indexOf('<!--p') === -1) {
+        if (setTo !== 'play' && (play_state.indexOf('pause') > -1 || play_state.indexOf('<!--p') === -1)) {
             document.getElementById('btn-graph-play').innerHTML = '<!--play--><svg xmlns="http://www.w3.org/2000/svg" width="14" height="15"><path d="M4,3 L4,11 10,7 Z" style="stroke:#fff;stroke-width:1;fill:#fff;"/></svg>';
-            graphStartStop('pause');
+            if (!setTo) {
+                graphStartStop('pause');
+            }
         } else {
             document.getElementById('btn-graph-play').innerHTML = '<!--pause--><svg xmlns="http://www.w3.org/2000/svg" width="14" height="15"><path d="M5.5,2 L4,2 4,11 5.5,11 Z M8.5,2 L10,2 10,11 8.5,11 Z" style="stroke:#fff;stroke-width:1;fill:#fff;"/></svg>';
-            if (!graph_interval_id) {
+            if (!graph_interval_id && !setTo) {
                 graphStartStop('play');
             }
         }
