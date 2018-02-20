8837c95be61d2176200c66edd0887ec7edd5ec47
Jim Ewald <jim.ewald@gmail.com>
2018-02-20 10:11:44 -0800

    Remove unnecessary import.

--- src/main/java/com/parallax/server/blocklyprop/servlets/MessageOfTheDayServlet.java
+++ src/main/java/com/parallax/server/blocklyprop/servlets/MessageOfTheDayServlet.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.parallax.server.blocklyprop.db.dao.MotdDao;
-import com.parallax.server.blocklyprop.db.dao.UserDao;
 import com.parallax.server.blocklyprop.db.generated.tables.records.MotdRecord;
 
 /**
