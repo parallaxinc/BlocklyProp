7c57092f13c7e61b184639aed348a658d38d7849
Jim Ewald <jim.ewald@gmail.com>
2018-02-20 10:55:24 -0800

    Update Message of the Day table.

--- db-updates/0009-motd.sql
+++ db-updates/0009-motd.sql
@@ -5,14 +5,16 @@
 CREATE TABLE IF NOT EXISTS blocklyprop.motd
 (
   id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  message_text VARCHAR(250) NOT NULL,
-  message_html VARCHAR(250),
-  notes VARCHAR(250),
-  enabled BOOLEAN DEFAULT FALSE  NOT NULL,
+  message_text VARCHAR(2000) NOT NULL,
+  message_html VARCHAR(2000),
+  notes VARCHAR(4000),
+  enabled BOOLEAN DEFAULT FALSE NOT NULL,
+  is_deleted BOOLEAN DEFAULT FALSE NOT NULL,
   enable_datetime DATETIME,
   disable_datetime DATETIME,
   create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  last_change_date TIMESTAMP
-) ENGINE=InnoDB
+  last_change_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) COMMENT 'Customer-facing alerts'
+  ENGINE=InnoDB
   AUTO_INCREMENT=0
   DEFAULT CHARSET=utf8;
