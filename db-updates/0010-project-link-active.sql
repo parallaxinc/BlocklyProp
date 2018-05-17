7b23e34b14ff5c5f414c4717ce621371b2960732
Jim Ewald <jim.ewald@gmail.com>
2018-05-17 13:03:39 -0700

    Add database schema change to support new shared project link.

--- db-updates/0010-project-link-active.sql
+++ db-updates/0010-project-link-active.sql
@@ -9,7 +9,14 @@
  *
  * Add an active field to the project link table. This will allow a user to
  * enable/disable their shared project without destroying the shared project
- *  key.
+ * key.
  */
 
 ALTER TABLE project_sharing ADD active BIT DEFAULT 0 NULL AFTER sharekey;
+
+/* 
+ * All of the current rows in the project_sharing table are active. Set the
+ * active flag for all records to match that state.
+ */
+UPDATE blocklyprop.project_sharing
+    SET active = true;
