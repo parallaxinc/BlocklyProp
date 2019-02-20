a90999e7e9a4f8471c92dd3981cb826152c93d4f
Jim Ewald <jewald@parallax.com>
2019-02-20 01:26:11 -0800

    Add inline documentation. Insert copyright notice.

--- src/main/java/com/parallax/server/blocklyprop/servlets/PrivacyPolicyServlet.java
+++ src/main/java/com/parallax/server/blocklyprop/servlets/PrivacyPolicyServlet.java
@@ -1,40 +1,53 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (c) 2019 Parallax Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the “Software”), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
+
 package com.parallax.server.blocklyprop.servlets;
 
 import com.google.inject.Singleton;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 /**
+ * Return the privacy policy page
  *
  * @author developer
  */
 @Singleton
 public class PrivacyPolicyServlet extends HttpServlet {
-    
-    /**
-     * Application logging system access
-     */
-     private static Logger LOG = LoggerFactory.getLogger(PrivacyPolicyServlet.class);
-     
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    // Application logging system access
+    private static final Logger LOG = LoggerFactory.getLogger(PrivacyPolicyServlet.class);
+
     /**
      * Handles the HTTP <code>GET</code> method.
      *
      * @param request  servlet request
      * @param response servlet response
-     *
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException      if an I/O error occurs
      */
@@ -43,10 +56,10 @@ public class PrivacyPolicyServlet extends HttpServlet {
             throws ServletException, IOException {
 
         LOG.debug("Requesting child privacy page");
-        
+
         request.getRequestDispatcher(
                 "WEB-INF/servlet/coppa/privacy-policy.jsp")
-                    .forward(request, response);
+                .forward(request, response);
     }
 
     /**
@@ -57,6 +70,5 @@ public class PrivacyPolicyServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Parallax COPPA policy page";
-    }// </editor-fold>
-
+    }
 }
